a3f541512fee713709e3b3adcc450654a7a253fb
chepetto <maggo@ausserferner.com>
2013-12-08 19:37:07 +0100

    - removed tabs

--- notes-ui/src/main/webapp/js/editors.js
+++ notes-ui/src/main/webapp/js/editors.js
@@ -7,11 +7,6 @@ $.widget("notes.editors", {
     _create: function () {
         var $this = this;
 
-        $this.doc2Tab = {};
-        $this.nav = $('<ul/>', {style: 'border-right:hidden; border-left:hidden; border-top:hidden; background:none; border-radius:0;'}).appendTo($this.element);
-        $this.uniqueId = parseInt(Math.random() * 10000);
-
-        $this.element.tabs();
 //        // -- events
 //        setInterval(function () {
 //
@@ -53,67 +48,21 @@ $.widget("notes.editors", {
         }));
     },
 
-    _getUniqueId: function () {
-        return this.uniqueId++;
-    },
-
     loadDocument: function (settings, model) {
         var $this = this;
 
-        var tabExists = model.has('id') && !(typeof($this.doc2Tab[model.get('id')]) === 'undefined');
+        var $content = $('<div/>');
 
-        if (tabExists) {
-            // open requested tab
-            $this.element.tabs({active: $this.doc2Tab[model.get('id')]});
-        } else {
-
-            var maxTitleLen = 25;
-
-            var documentId = model.has('id') ? model.get('id') : $this._getUniqueId();
-
-            var tabTitle = model.has('title') ? model.get('title') : 'New';
-            var shortTabTitle = tabTitle;
-            if (shortTabTitle.length > maxTitleLen) {
-                shortTabTitle = shortTabTitle.substr(0, maxTitleLen / 2) + '..' + shortTabTitle.substr(shortTabTitle.length - maxTitleLen / 2, shortTabTitle.length);
-            }
-
-            var uniqueId = 'document-' + documentId;
-            var $tabContent = $('<div/>', {id: uniqueId, class: 'container'});
-
-            var $tabHeader = $('<li/>').append(
-                    $('<a/>', {href: '#' + uniqueId, text: shortTabTitle, title: tabTitle})
-                ).append(
-                    $('<span/>', {class: 'ui-icon ui-icon-close', role: 'presentation', text: 'Remove Tab'}).click(function () {
-                        $tabContent.remove();
-                        delete $this.doc2Tab[documentId];
-                        $tabHeader.remove();
-
-                        // todo select other tab if available
-//                        var newTabId;
-//                        for(newTabId in $this.doc2Tab) break;
-//
-//                        $this.element.tabs( {active: newTabId} );
-                    })
-                );
-            $this.nav.append($tabHeader);
-
-            switch (settings.kind.toLowerCase().trim()) {
-                case 'text':
-                    $tabContent.texteditor({model: model});
-                    break;
-                case 'pdf':
-                    $tabContent.pdfeditor({model: model});
-                    break;
-            }
-
-            var tabId = $.map($this.doc2Tab,function (n, i) {
-                return i;
-            }).length;
-            $this.doc2Tab[documentId] = tabId;
-            $this.element.append($tabContent);
-            $this.element.tabs("refresh");
-            $this.element.tabs({active: tabId});
+        switch (settings.kind.toLowerCase().trim()) {
+            case 'text':
+                $content.texteditor({model: model});
+                break;
+            case 'pdf':
+                $content.pdfeditor({model: model});
+                break;
         }
+
+        $this.element.empty().append($content);
 
     }
 })
