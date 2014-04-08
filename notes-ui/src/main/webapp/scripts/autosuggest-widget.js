ee2fac699f18c837ee7fcfc485f98da79304565d
damoeb <maggo@ausserferner.com>
2014-04-08 09:09:56 +0200

    - autosuggest ui

--- notes-ui/src/main/webapp/scripts/autosuggest-widget.js
+++ notes-ui/src/main/webapp/scripts/autosuggest-widget.js
@@ -16,14 +16,14 @@ $.widget('notes.autosuggest', {
             prefetch: {
                 url: '/notes/rest/search/history',
                 filter: function (response) {
-//                    notes.util.sortJSONArrayDESC(response.result, 'lastUsed')
+                    notes.util.sortJSONArrayDESC(response.result, 'lastUsed')
                     return response.result;
                 }
             },
             remote: {
                 url: '/notes/rest/search/suggest/?q=%QUERY',
                 filter: function (response) {
-//                    notes.util.sortJSONArrayDESC(response.result, 'lastUsed');
+                    notes.util.sortJSONArrayDESC(response.result, 'lastUsed');
                     return response.result;
                 }
             }
@@ -39,46 +39,17 @@ $.widget('notes.autosuggest', {
             name: 'queries',
             displayKey: 'value',
             source: queries.ttAdapter()
-        });
-
-
-//        var suggestionLayer = $('<ul class="dropdown-menu" style="width: 100%"></ul>');
-//        $this.element.parent().append(suggestionLayer);
-//        $this.suggestionLayer = suggestionLayer;
-
-//        var onSuccess = function (history) {
-//
-//            notes.util.sortJSONArrayDESC(history, 'lastUsed')
-//
-//            console.log('logged queries: ' + history.length);
-//
-//            var reformatted = [];
-//            for(var i=0; i<history.length; i++) {
-//                var item = history[i];
-//                reformatted.push(item.query);
-//            }
-//
-//
-//            $this.history = reformatted;
-//
-//            $this.option('source', reformatted);
-//        };
-//
-//        var onError = function () {
-//            console.error('Cannot load query-history');
-//        };
-//
-//        notes.util.jsonCall('GET', REST_SERVICE + '/search/history', null, null, onSuccess, onError);
-//
-//
-//        $this.element.unbind('focus').focus(function (e) {
-//            $this.option('source', $this.history);
-//            console.log('show suggestion layer');
-//            $this.search('');
-//        });
-//
-//        return this._super();
-//    },
+        }).on('typeahead:selected',function (event, query) {
+                var val = query.value;
+                console.log('trigger search ' + val);
+                notes.queries.find(val);
+            }).keypress(function (e) {
+                if (e.which == 13) {
+                    console.log('trigger search');
+                    $(this).typeahead('close');
+                    notes.queries.find();
+                }
+            });
     }
 
 });
